7c1b9363e564dc68871108391f3ad40ee90a0985
Your Name <you@example.com>
2023-03-18 20:20:15 +0700

    codee

---
+++ Controllers/loginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ictshop.Areas.Admin.Controllers
+{
+    public class loginController : Controller
+    {
+        // GET: Admin/login
+        public ActionResult Dangnhap()
+        {
+            return View();
+        }
+    }
+}
